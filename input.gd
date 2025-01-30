6beccda7a98759e4e101deb8a414b3d5aec1be2b
Faustine <Faustine@JUJODESK>
2025-01-30 19:49:36 +0100

    midi merde pourri caca

---
+++ input.gd
@@ -0,0 +1,26 @@
+extends Node
+
+func _ready():
+	OS.open_midi_inputs()
+	print(OS.get_connected_midi_inputs())
+
+	#InputMap.action_add_event("face_nose",)
+
+# func _input(input_event):
+# 	if input_event is InputEventMIDI:
+# 		_print_midi_info(input_event)
+
+func _print_midi_info(midi_event):
+	print(midi_event)
+	print("Channel ", midi_event.channel)
+	print("Message ", midi_event.message)
+	print("Pitch ", midi_event.pitch)
+	print("Velocity ", midi_event.velocity)
+	print("Instrument ", midi_event.instrument)
+	print("Pressure ", midi_event.pressure)
+	print("Controller number: ", midi_event.controller_number)
+	print("Controller value: ", midi_event.controller_value)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMIDI:
+		print(event.controller_number, ": ", event.controller_value)
